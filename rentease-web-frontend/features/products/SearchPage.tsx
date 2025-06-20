d29af6c7e3bb87436323275e8badc520dc5f72a0
65011211019 <138645306+65011211019@users.noreply.github.com>
2025-06-20 23:43:50 +0700

    Fix all API baseURL to /api for production deployment

--- rentease-web-frontend/features/products/SearchPage.tsx
+++ rentease-web-frontend/features/products/SearchPage.tsx
@@ -72,7 +72,7 @@ export const SearchPage: React.FC = () => {
         setProvinces(provs.data);
 
         // Fetch all products
-        const response = await fetch('http://localhost:3001/api/products');
+        const response = await fetch('/api/products');
         if (!response.ok) {
           throw new Error('Failed to fetch products');
         }
